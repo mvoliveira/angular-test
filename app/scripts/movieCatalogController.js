94bbed7057eca8bf7fa4ca27d68c337360046331
Matheus <matheus.mvo10@gmail.com>
2017-11-16 12:09:29 -0200

    Modal

--- app/scripts/movieCatalogController.js
+++ app/scripts/movieCatalogController.js
@@ -1,13 +1,12 @@
 angular.module('movieCatalog')
-.controller('firstCtrl', function($scope, myCatalog) {
+.controller('movieCatalogController', function($scope, myCatalog) {
   let loadMovies = function(){
     myCatalog.showMovies().then(function(movies) {
       $scope.movies = movies
     })
   }
-  $scope.title = "Movie Catalog"
 
-loadMovies();
-//
-//
+  loadMovies();
+  //
+  //
 });
