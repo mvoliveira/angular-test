10ecca30f0cae01958d757306d274aa2c5965d23
Matheus <matheus.mvo10@gmail.com>
2017-11-16 14:53:55 -0200

    remove empty spaces

--- app/scripts/myCatalog.factory.js
+++ app/scripts/myCatalog.factory.js
@@ -3,24 +3,16 @@ angular.module("movieCatalog")
 	return {
 		showMovies: function() {
 			var promessa = $q.defer();
-
 			$http.get("http://private-74b50-provaangularjs.apiary-mock.com/movies").then(
 				function(result){
 					var movies = []
-
 					angular.forEach(result, function(movie){
-						// movie.id = id;
-						// movies.push(movie);
-					movies = result.data
-
+						movies = result.data
 					});
-
 					promessa.resolve(movies);
 				}
 			);
-
 			return promessa.promise;
 		},
-
 	};
 });
